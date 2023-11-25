9b2f96af4645014b5b9b7ae8f6ef54fe9edc7012
ihadi <iman.haris.hadi@intel.com>
2023-11-26 00:43:16 +0800

    updated stylings for industry and financial risk

--- wonderlend-fe/src/app/applicant/components/IndustryScore.tsx
+++ wonderlend-fe/src/app/applicant/components/IndustryScore.tsx
@@ -3,15 +3,16 @@ const IndustryScore = () => {
         <div className="max-w-xs p-4 bg-white rounded-lg border shadow-sm">
             <h3 className="text-md font-semibold text-gray-800">Industry Score</h3>
             <div className="flex items-center mt-2">
-                <div className="text-2xl font-bold text-gray-800 mr-2">80%</div>
-                <div className="text-sm font-semibold text-green-500 flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
-                    </svg>
+                <div className="flex-grow-1 text-2xl font-bold text-gray-800 mr-2">80%</div>
+            </div>
+            <div className="flex">
+                <div className="text-sm font-semibold rounded-full p-1 bg-green-200 text-green-500 flex items-center">
+                    ^
                     18%
                 </div>
+                &nbsp;
+                <p className="text-sm text-gray-600 mt-1">from last quarter</p>
             </div>
-            <p className="text-sm text-gray-600 mt-1">from last quarter</p>
         </div>
     );
 }
