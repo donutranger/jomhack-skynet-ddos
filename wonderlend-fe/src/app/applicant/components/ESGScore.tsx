43df4387a579bcb6d7093e8e73a8b85f876f0bb8
ihadi <iman.haris.hadi@intel.com>
2023-11-26 00:51:49 +0800

    fixed donut

--- wonderlend-fe/src/app/applicant/components/ESGScore.tsx
+++ wonderlend-fe/src/app/applicant/components/ESGScore.tsx
@@ -1,20 +1,44 @@
 const ESGScore = () => {
-    const strokeDasharray = 2 * Math.PI * 50; // This should be the circumference of the circle (2 * π * radius)
-    const strokeDashoffset = strokeDasharray - strokeDasharray * 7 / 10; // Assuming the score is 7 out of 10 for ESG Score
+    const score = 7; // Dynamically change this
+    const circumference = 2 * Math.PI * 40;
+    const strokeDasharray = circumference;
+    const strokeDashoffset = circumference - (score / 10) * circumference;
 
     return (
-        <div className="flex flex-col items-center justify-center p-4 bg-white rounded-lg border shadow-sm w-44 h-24">
-            <svg width="100" height="100" viewBox="0 0 100 100" className="transform -rotate-90">
-                <circle cx="50" cy="50" r="45" fill="transparent" stroke="#f3f4f6" strokeWidth="10" />
-                <circle cx="50" cy="50" r="45" fill="transparent" stroke="#10b981" strokeWidth="10"
-                    strokeDasharray={strokeDasharray}
-                    strokeDashoffset={strokeDashoffset}
-                />
-            </svg>
-            <div className="absolute">
-                <span className="text-3xl font-bold text-gray-800">7</span>
+        <div className="rounded-lg border shadow-sm w-60 h-auto relative bg-white flex flex-row items-center">
+            <div className="flex flex-col items-left p-4 min-w">
+                <div className="text-xl font-semibold">ESG Score</div>
+                <div className="text-sm text-gray-500">Overall</div>
             </div>
-            <div className="text-sm text-gray-500">Overall</div>
+            <div className="flex items-center justify-center p-4">
+                <svg
+                    width="100%"
+                    height="100%"
+                    viewBox="0 0 100 100"
+                    className="transform -rotate-90"
+                >
+                    <circle
+                        cx="50"
+                        cy="50"
+                        r="40"
+                        fill="transparent"
+                        stroke="#f3f4f6"
+                        strokeWidth="10"
+                    />
+                    <circle
+                        cx="50"
+                        cy="50"
+                        r="40"
+                        fill="transparent"
+                        stroke="#10b981"
+                        strokeWidth="10"
+                        strokeDasharray={strokeDasharray}
+                        strokeDashoffset={strokeDashoffset}
+                        strokeLinecap="round"
+                    />
+                </svg>
+                <div className="absolute text-3xl font-bold text-gray-800">{score}</div>
+            </div>
         </div>
     );
 }
