7ed24745ab2c14ed456bb409162be8dee37150f1
ihadi <iman.haris.hadi@intel.com>
2023-11-26 00:59:28 +0800

    updated repayment condifence component

--- wonderlend-fe/src/app/applicant/components/RepaymentConfidence.tsx
+++ wonderlend-fe/src/app/applicant/components/RepaymentConfidence.tsx
@@ -1,14 +1,17 @@
 const RepaymentConfidence = () => {
-    const confidenceLevel = '88%'; // Example confidence level, this can be dynamic
-    const confidenceColor = 'bg-green-500'; // Set the color based on the level of confidence
+    const confidenceLevel = 88; // Dynamic
+    const getTextColorClass = (level: number) => {
+        if (level < 50) return 'text-red-600';
+        if (level < 80) return 'text-yellow-400';
+        return 'text-green-500';
+    };
+    const textColorClass = getTextColorClass(confidenceLevel);
 
     return (
-        <div className="max-w-xs px-4 py-2 bg-white rounded-lg border shadow-sm">
+        <div className="max-w-sm px-4 py-2 bg-white rounded-lg border shadow-sm">
             <h3 className="text-md font-semibold text-gray-800">Confidence of Repayment</h3>
-            <div className="text-2xl font-bold text-gray-800 my-2">{confidenceLevel}</div>
-            <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                <div className={`${confidenceColor} h-2.5 rounded-full`} style={{ width: confidenceLevel }}></div>
-            </div>
+            <div className={`text-2xl font-bold ${textColorClass} my-2`}>{confidenceLevel}%</div>
+            
             <div className="flex justify-between text-xs text-gray-600 mt-2">
                 <span className="flex items-center">
                     <span className="h-2 w-2 bg-red-600 rounded-full mr-1"></span>
